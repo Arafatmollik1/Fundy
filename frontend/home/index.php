fd342376b3c331c009a967ece525d1487faa07f7
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2024-01-06 13:32:16 +0200

    made the card

--- frontend/home/index.php
+++ frontend/home/index.php
@@ -1,7 +1,22 @@
-<?php
- echo "<pre>";
- var_dump($_SESSION);
- echo "</pre>";
-?>
 <?php include 'views/header.php'; ?>
-<h1>home</h1>
+<div class="container  vh-100 d-flex justify-content-center align-items-center">
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="assets/pictures/example_pic.JPG" alt="Card image cap">
+    <div class="card-body">
+        <div class="mb-5">
+            <h5 class="card-title">Celebrating victory day</h5>
+            <p class="text-secondary fs-6">Let’s celebrate 16th December, victory day.</p>
+        </div>
+        <hr class="hr">
+        <div class="ticket-price">
+            <div class="tag-holder">
+                <span > Price </span>
+            </div>
+            <p class="m-0"> €8 </p>
+        </div>
+        <div class="text-center w-100">
+            <a href="#" class="btn btn-success">Buy ticket</a>
+        </div>
+    </div>
+    </div>
+</div>
--- views/header.php
+++ views/header.php
@@ -1,4 +1,4 @@
-<nav class="justify-content-end px-md-5 navbar navbar-expand-lg">
+<nav class="justify-content-end px-2 px-md-5 navbar navbar-expand-lg">
     <button id="themeSwitcher" class="btn rounded fs-6">
         <i class="bi bi-brightness-high"></i><span class="px-2"></span>
     </button>
