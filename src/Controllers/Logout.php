2985a26504392f678de694ad705e70f249f0939c
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2024-01-07 17:00:24 +0200

    fix logout contorller

--- src/Controllers/Logout.php
+++ src/Controllers/Logout.php
@@ -18,7 +18,7 @@ class Logout {
         if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
             $googleAuth->revokeToken($_SESSION['access_token']);
         }
-        unset($_SESSION);
+        session_unset();
         $commonHelper = new Common();
         $commonHelper->internalRedirect('logout/index');
         exit();
